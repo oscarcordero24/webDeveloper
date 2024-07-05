9c36c66db7b36b795a6110bb4d654f926ad1a139
Oscar Cordero <oscar.cordero201318@gmail.com>
2024-07-04 22:32:31 -0500

    New Project Created -ModernUI

---
+++ ModernUI/javascript/script.js
@@ -0,0 +1,14 @@
+
+/* window.onscroll = () => {
+    let top = window.scrollY; // Top of the screen
+    let offset = sec.offsetTop - 150; // Top location of the section
+    let height = sec.offsetHeight; // Height of the section
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        })
+    }
+} */
